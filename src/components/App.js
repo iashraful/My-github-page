66771b25a443eacee5d13a36185298b68ccc9135
Ashraful Islam <ashraful@field.buzz>
2017-04-25 10:40:41 +0600

    Added bootstrap from cdn

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,8 @@ import '../styles/App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="jumbotron">
+        <h1 className="text-center">Hello, World</h1>
       </div>
     );
   }
