c51e5a73e75feffae70edcd58fca044df02669e8
Ashraful Islam <ashraful@field.buzz>
2017-04-26 15:03:34 +0600

    Added a static nav bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
 import '../styles/App.css';
+
+import NavigationBar from './NavigationBar'
 
 class App extends Component {
   render() {
     return (
-      <div className="jumbotron">
-        <h1 className="text-center">Hello, World</h1>
+      <div>
+        <NavigationBar options="{'brand': 'Ashraful Islam'}"/>
+        <div className="jumbotron">
+          <h1 className="text-center">Hello, World</h1>
+        </div>
       </div>
     );
   }
