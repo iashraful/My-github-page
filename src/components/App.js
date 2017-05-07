daf8492e966b71ff5bad8747139ea647b15da8cb
Ashraful Islam <ashraful@field.buzz>
2017-05-07 09:36:24 +0600

    Code Refactored. Update Navigation bar dynamically

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,48 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../styles/App.css';
 
 import NavigationBar from './NavigationBar'
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <NavigationBar options="{'brand': 'Ashraful Islam'}"/>
-        <div className="jumbotron">
-          <h1 className="text-center">Hello, World</h1>
-        </div>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            brand: 'Ashraful Islam',
+            items: [
+                {
+                    id: 1,
+                    name: 'Home',
+                    url: '#/home'
+                },
+                {
+                    id: 2,
+                    name: 'About',
+                    url: '#/about'
+                },
+                {
+                    id: 3,
+                    name: 'Contact',
+                    url: '#/contact'
+                },
+                {
+                    id: 4,
+                    name: 'Portfolio',
+                    url: '#/portfolios'
+                },
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <NavigationBar options={this.state}/>
+                <div className="jumbotron">
+                    <h1 className="text-center">Hello, World</h1>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,43 +1,35 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class NavigationBar extends Component {
-	render() {
-		return (
-			<div>
-			    <nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
-			        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-			        <span className="navbar-toggler-icon"></span>
-			        </button>
-			        <a className="navbar-brand" href="#">{this.props.options.brand}</a>
-			        <div className="collapse navbar-collapse" id="navbarsExampleDefault">
-			            <ul className="navbar-nav mr-auto">
-			                <li className="nav-item active">
-			                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-			                </li>
-			                <li className="nav-item">
-			                    <a className="nav-link" href="#">Link</a>
-			                </li>
-			                <li className="nav-item">
-			                    <a className="nav-link disabled" href="#">Disabled</a>
-			                </li>
-			                <li className="nav-item dropdown">
-			                    <a className="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-			                    <div className="dropdown-menu" aria-labelledby="dropdown01">
-			                        <a className="dropdown-item" href="#">Action</a>
-			                        <a className="dropdown-item" href="#">Another action</a>
-			                        <a className="dropdown-item" href="#">Something else here</a>
-			                    </div>
-			                </li>
-			            </ul>
-			            <form className="form-inline my-2 my-lg-0">
-			                <input className="form-control mr-sm-2" type="text" placeholder="Search" />
-			                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-			            </form>
-			        </div>
-			    </nav>
-			</div>
-		)
-	}
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
+                    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                            data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <a className="navbar-brand" href="#">{this.props.options.brand}</a>
+                    <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+                        <ul className="navbar-nav mr-auto">
+
+                            {this.props.options.items.map(item =>
+                                <li className="nav-item"
+                                    key={item.id}> {/* map require unique key. So, I just put. Nothing special */}
+                                    <a className="nav-link" href={item.url}>{item.name}</a>
+                                </li>
+                            )}
+                        </ul>
+                        <form className="form-inline my-2 my-lg-0">
+                            <input className="form-control mr-sm-2" type="text" placeholder="Search"/>
+                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
 }
 
 export default NavigationBar;
