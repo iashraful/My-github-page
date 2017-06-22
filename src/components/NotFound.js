03f2bae0657994b79d6ed94785f25dc7f9cc2694
Ashraful Islam <ashrafulrobin3@gmail.com>
2017-06-22 07:07:29 +0600

    Minor styles updated

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -12,20 +12,20 @@ class NotFound extends Component {
                 <div className="row">
                     <div className="col-md-12">
                         <div className="error-template">
-                            <h1>
-                                Oops!</h1>
-                            <h2>
-                                404 Not Found</h2>
+                            <h1>Oops!</h1>
+                            <h2>404 Not Found</h2>
                             <div className="error-details">
                                 Sorry, an error has occured, Requested page not found!
                             </div>
                             <div className="error-actions">
                                 <Link to="/home" className="btn btn-outline-success btn-md" role="button">
-                                    <i className="fa fa-home p-r-10"/> Take Me Home
+                                    <i className="fa fa-home p-r-10"/>
+                                    Take Me Home
                                 </Link>
                                 <a href="mailto:ashrafulrobin3@gmail.com?Subject=404%20Not%20Found" target="_top"
                                    className="btn btn-outline-primary btn-md">
-                                    <i className="fa fa-envelope p-r-10"/> Mail to Author
+                                    <i className="fa fa-envelope p-r-10"/>
+                                    Mail to Author
                                 </a>
                             </div>
                         </div>
